ce32a1963ce7edb791b9f475947bf47e15578889
framic23 <miccolis@philae.polito.it>
2022-02-25 20:11:49 +0100

    Bug fix

--- DEEPrior/lib/evaluate_fusions.py
+++ DEEPrior/lib/evaluate_fusions.py
@@ -1,9 +1,10 @@
 import os
 from pathlib import Path
+from xml.dom.minidom import Attr
 import numpy as np
 import pandas as pd
 from keras.models import load_model
-from lib.deep_utils import load_tokenizer,retrieve_test_sequences
+from lib.deep_utils import load_tokenizer, retrieve_test_sequences
 
 MIN_PROT_SEQ = 5
 MAX_PROT_SEQ = 4000
@@ -27,10 +28,12 @@ def evaluate_fusions(list_fusions, trained_model_path, output_file_path):
     y_test = []
     i = 0
     # 3. for each fusion in the list, extract the protein sequences
-    toskip = [f for f, fusion in enumerate(list_fusions) if (fusion == 'Missing information, fusion skipped' or (fusion.chr5p == 'Invalid value' or fusion.chr3p == 'Invalid value'))]
+    toskip = [f for f, fusion in enumerate(list_fusions) if (fusion == 'Missing information, fusion skipped' or (
+        fusion.chr5p == 'Invalid value' or fusion.chr3p == 'Invalid value'))]
     for fusion in list_fusions:
         i = i+1
-        print("Deep learning processing: %0.2f%% completed" % (i/len(list_fusions)*100))
+        print("Deep learning processing: %0.2f%% completed" %
+              (i/len(list_fusions)*100))
         if (i-1) in toskip:
             df_res.loc[df_res.shape[0]] = [None] * len(df_res_columns)
             if fusion == 'Missing information, fusion skipped':
@@ -40,7 +43,8 @@ def evaluate_fusions(list_fusions, trained_model_path, output_file_path):
                 df_res.FusionPair[i - 1] = 'Invalid input data'
                 df_res.OncogenicProbability[i - 1] = 'fusion skipped'
         else:
-            x_test, sequences, label = retrieve_test_sequences(fusion, tokenizer)
+            x_test, sequences, label = retrieve_test_sequences(
+                fusion, tokenizer)
             if label == -1:
                 df_res = df_res.append({'FusionPair': 'No protein produced',
                                         'OncogenicProbability': 'Not Applicable',
@@ -92,33 +96,61 @@ def evaluate_fusions(list_fusions, trained_model_path, output_file_path):
             if len(x_test) > 0:
                 y_test.append(label)
                 labels_pred = model.predict(x_test)
-                predictions = [pred[0].astype(np.float) for pred in labels_pred]
+                predictions = [pred[0].astype(np.float)
+                               for pred in labels_pred]
 
                 # 5. save the results on the final file
-                df_res = df_res.append({'FusionPair': fusion.fusion_pair,
-                                        'OncogenicProbability': max(predictions),
-                                        'Version': fusion.version,
-                                        'Chr5p': fusion.chr5p,
-                                        'Coord5p': fusion.coord5p,
-                                        '5pStrand': fiveprime_strand,
-                                        '5pCommonName': fusion.portions[0].genes[0].common_name,
-                                        '5pEnsg': fusion.portions[0].ensg,
-                                        '5pGeneFunctionality': fusion.protein_cod[0],
-                                        '5pGeneDescription': fusion.portions[0].genes[0].description,
-                                        'Chr3p': fusion.chr3p,
-                                        'Coord3p': fusion.coord3p,
-                                        '3pStrand': threeprime_strand,
-                                        '3pCommonName': fusion.portions[1].genes[0].common_name,
-                                        '3pEnsg': fusion.portions[1].ensg,
-                                        '3pGeneFunctionality': fusion.protein_cod[1],
-                                        '3pGeneDescription': fusion.portions[1].genes[0].description,
-                                        'MainProteinLength': fusion.main_protein_len,
-                                        'TruncatedProtein': fusion.early_stop,
-                                        '5p_gene_complete': fusion.complete_5p,
-                                        '3p_gene_complete': fusion.complete_3p,
-                                        'MainProtein': main_protein_seq
-                                        # 'Proteins': sequences
-                                        }, ignore_index=True)
+                try:
+                    df_res = df_res.append({'FusionPair': fusion.fusion_pair,
+                                            'OncogenicProbability': max(predictions),
+                                            'Version': fusion.version,
+                                            'Chr5p': fusion.chr5p,
+                                            'Coord5p': fusion.coord5p,
+                                            '5pStrand': fiveprime_strand,
+                                            '5pCommonName': fusion.portions[0].genes[0].common_name,
+                                            '5pEnsg': fusion.portions[0].ensg,
+                                            '5pGeneFunctionality': fusion.protein_cod[0],
+                                            '5pGeneDescription': fusion.portions[0].genes[0].description,
+                                            'Chr3p': fusion.chr3p,
+                                            'Coord3p': fusion.coord3p,
+                                            '3pStrand': threeprime_strand,
+                                            '3pCommonName': fusion.portions[1].genes[0].common_name,
+                                            '3pEnsg': fusion.portions[1].ensg,
+                                            '3pGeneFunctionality': fusion.protein_cod[1],
+                                            '3pGeneDescription': fusion.portions[1].genes[0].description,
+                                            'MainProteinLength': fusion.main_protein_len,
+                                            'TruncatedProtein': fusion.early_stop,
+                                            '5p_gene_complete': fusion.complete_5p,
+                                            '3p_gene_complete': fusion.complete_3p,
+                                            'MainProtein': main_protein_seq
+                                            # 'Proteins': sequences
+                                            }, ignore_index=True)
+                except:
+                    df_res = df_res.append({'FusionPair': '',
+                                            'OncogenicProbability': '',
+                                            'Version': '',
+                                            'Chr5p': '',
+                                            'Coord5p': '',
+                                            '5pStrand': '',
+                                            '5pCommonName': '',
+                                            '5pEnsg': '',
+                                            '5pGeneFunctionality': '',
+                                            '5pGeneDescription': '',
+                                            'Chr3p': '',
+                                            'Coord3p': '',
+                                            '3pStrand': '',
+                                            '3pCommonName': '',
+                                            '3pEnsg': '',
+                                            '3pGeneFunctionality': '',
+                                            '3pGeneDescription': '',
+                                            'MainProteinLength': '',
+                                            'TruncatedProtein': '',
+                                            '5p_gene_complete': '',
+                                            '3p_gene_complete': '',
+                                            'MainProtein': ''
+                                            # 'Proteins': sequences
+                                            }, ignore_index=True)
+
             # else:
             #     try:
             #         df_res = df_res.append({'FusionPair': fusion.fusion_pair,
@@ -173,5 +205,3 @@ def evaluate_fusions(list_fusions, trained_model_path, output_file_path):
     # 6. save the results to file
     df_res.to_csv(output_file_path, sep='\t')
     print("Results saved at: "+output_file_path)
-
-
